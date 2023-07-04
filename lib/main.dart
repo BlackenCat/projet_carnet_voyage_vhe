a43ed5ead4a10bcfb12b1e29d2639869c2bec024
vincent <vincent.hebert37@gmail.com>
2023-07-04 09:30:49 +0200

    Mise à jour

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,200 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:weather/weather.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(TravelJournalApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class TravelJournalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Travel Journal',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: TravelJournalScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TravelJournalScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TravelJournalScreenState createState() => _TravelJournalScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TravelJournalScreenState extends State<TravelJournalScreen> {
+  List<TravelEntry> travelEntries = [];
+  TextEditingController locationController = TextEditingController();
+  TextEditingController commentController = TextEditingController();
+  late DateTime selectedDate;
+  late String selectedImagePath;
+  WeatherFactory weatherFactory = WeatherFactory("YOUR_API_KEY", language: Language.FRENCH);
 
-  void _incrementCounter() {
+  Future<void> selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await DatePicker.showDatePicker(
+      context,
+      locale: LocaleType.fr,
+      currentTime: selectedDate ?? DateTime.now(),
+    );
+
+    if (pickedDate != null && pickedDate != selectedDate) {
+      setState(() {
+        selectedDate = pickedDate;
+      });
+    }
+  }
+
+  Future<void> selectImage(BuildContext context) async {
+    final ImagePicker _picker = ImagePicker();
+    final XFile? pickedImage = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedImage != null) {
+      setState(() {
+        selectedImagePath = pickedImage.path;
+      });
+    }
+  }
+
+  Future<Weather> fetchWeather(String location) async {
+    Weather weather = await weatherFactory.currentWeatherByCityName(location);
+    return weather;
+  }
+
+  void addTravelEntry() async {
+    if (selectedDate == null || locationController.text.isEmpty) {
+      return;
+    }
+
+    Weather weather = await fetchWeather(locationController.text);
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      travelEntries.add(
+        TravelEntry(
+          date: selectedDate,
+          location: locationController.text,
+          comment: commentController.text,
+          imagePath: selectedImagePath,
+          weather: weather,
+        ),
+      );
+      selectedDate = '' as DateTime;
+      locationController.clear();
+      commentController.clear();
+      selectedImagePath = 'null';
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Travel Journal'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: locationController,
+                    decoration: InputDecoration(
+                      labelText: 'Lieu visité',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.date_range),
+                  onPressed: () {
+                    selectDate(context);
+                  },
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: commentController,
+              decoration: InputDecoration(
+                labelText: 'Commentaire',
+              ),
             ),
-          ],
-        ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              selectImage(context);
+            },
+            child: Text('Ajouter une photo'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              addTravelEntry();
+            },
+            child: Text('Ajouter'),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: travelEntries.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text('Lieu : ${travelEntries[index].location}'),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Date : ${travelEntries[index].formattedDate}'),
+                      Text('Commentaire : ${travelEntries[index].comment}'),
+                      if (travelEntries[index].imagePath != null)
+                        Image.file(
+                          File(travelEntries[index].imagePath),
+                          width: 100,
+                          height: 100,
+                        ),
+                      FutureBuilder<Weather>(
+                        future: fetchWeather(travelEntries[index].location),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            final weather = snapshot.data;
+                            return Text('Météo : ${weather.temperature.celsius}°C, ${weather.weatherDescription}');
+                          } else if (snapshot.hasError) {
+                            return Text('Erreur de chargement de la météo');
+                          } else {
+                            return CircularProgressIndicator();
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class TravelEntry {
+  final DateTime date;
+  final String location;
+  final String comment;
+  final String imagePath;
+  final Weather weather;
+
+  TravelEntry({
+    required this.date,
+    required this.location,
+    required this.comment,
+    required this.imagePath,
+    required this.weather,
+  });
+
+  String get formattedDate {
+    return '${date.day}/${date.month}/${date.year}';
   }
 }
