03aec7ca9931b9ea0501653fa64c77f1b2fab80c
Beinstore <bastien.graveleau49@orange.fr>
2023-07-09 17:52:59 +0200

    login page

---
+++ lib/login_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  _email = value;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Email',
+              ),
+            ),
+            TextField(
+              onChanged: (value) {
+                setState(() {
+                  _password = value;
+                });
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: 'Mot de passe',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // Logique de connexion
+              },
+              child: Text('Connexion'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
